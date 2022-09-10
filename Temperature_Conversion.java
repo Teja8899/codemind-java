4b9d38c9ef826bc2689a9d0997609f714a63d317
ROTTE BALA SURYA VENKATA TEJA <99643609+Teja8899@users.noreply.github.com>
2022-09-10 11:26:35 +0530

    import java.util.*; class sol {     public static void main(String args[])     {         Scanner sc=new Scanner(System.in);         int a;         float b;         a=sc.nextInt();         b=32+((float)(a*9)/5);         System.out.format("%.2f",b);     } }

---
+++ Temperature_Conversion.java
@@ -0,0 +1,13 @@
+import java.util.*;
+class sol
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int a;
+        float b;
+        a=sc.nextInt();
+        b=32+((float)(a*9)/5);
+        System.out.format("%.2f",b);
+    }
+}
