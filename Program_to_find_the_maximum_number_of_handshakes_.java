532a9d1ff31c67e2bc462df13fa8b45069d8caaf
ROTTE BALA SURYA VENKATA TEJA <99643609+Teja8899@users.noreply.github.com>
2022-09-10 10:59:19 +0530

    import java.util.*; class sol {     public static void main(String args[])     {         Scanner sc=new Scanner(System.in);         int a;         a=sc.nextInt();         System.out.println((int)(a*(a-1)/2));     } }

---
+++ Program_to_find_the_maximum_number_of_handshakes_.java
@@ -0,0 +1,11 @@
+import java.util.*;
+class sol
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int a;
+        a=sc.nextInt();
+        System.out.println((int)(a*(a-1)/2));
+    }
+}
