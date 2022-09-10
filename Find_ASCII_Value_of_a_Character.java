47012ad78edc5eec1d0eaf46e5c42549078b7ba1
ROTTE BALA SURYA VENKATA TEJA <99643609+Teja8899@users.noreply.github.com>
2022-09-10 11:10:18 +0530

    import java.util.*; class sol {     public static void main(String args[])     {         Scanner sc=new Scanner(System.in);         char a;         a=sc.nextLine().charAt(0);         System.out.println((int)a);     } }

---
+++ Find_ASCII_Value_of_a_Character.java
@@ -0,0 +1,11 @@
+import java.util.*;
+class sol
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        char a;
+        a=sc.nextLine().charAt(0);
+        System.out.println((int)a);
+    }
+}
