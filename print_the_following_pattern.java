99fc3b61615f782bbf6d48505ae523d4cb6b298a
ROTTE BALA SURYA VENKATA TEJA <99643609+Teja8899@users.noreply.github.com>
2022-09-23 10:01:18 +0530

    import java.util.Scanner; class sol {     public static void main(String args[])     {         Scanner sc=new Scanner(System.in);         int n,i,j;         n=sc.nextInt();         for(i=n;i>=1;i--)         {             for(j=1;j<=i;j++)             {                 System.out.print(j);             }             System.out.println();         }         sc.close();     } }

---
+++ print_the_following_pattern.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+class sol
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int n,i,j;
+        n=sc.nextInt();
+        for(i=n;i>=1;i--)
+        {
+            for(j=1;j<=i;j++)
+            {
+                System.out.print(j);
+            }
+            System.out.println();
+        }
+        sc.close();
+    }
+}
