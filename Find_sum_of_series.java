4d064a653cf6c1ee98cb119865869308668556dc
ROTTE BALA SURYA VENKATA TEJA <99643609+Teja8899@users.noreply.github.com>
2022-09-26 22:56:52 +0530

    import java.util.Scanner; class sol {     public static void main(String args[])     {         Scanner sc=new Scanner(System.in);         int n;         n=sc.nextInt();         float s=0;         for(float i=1;i<n+1;i++)         {             s=s+(1/i);         }         System.out.format("%.2f",s);     }
    }

---
+++ Find_sum_of_series.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+class sol
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int n;
+        n=sc.nextInt();
+        float s=0;
+        for(float i=1;i<n+1;i++)
+        {
+            s=s+(1/i);
+        }
+        System.out.format("%.2f",s);
+    }
+    
+}
